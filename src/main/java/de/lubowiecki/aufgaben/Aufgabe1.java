2a4c559ab980e7fe6823ae70c5b08e6119d52442
tlubowiecki <tomasz@lubowiecki.de>
2026-05-18 14:36:50 +0200

    Aufgabe 1, Update

--- src/main/java/de/lubowiecki/aufgaben/Aufgabe1.java
+++ src/main/java/de/lubowiecki/aufgaben/Aufgabe1.java
@@ -2,5 +2,8 @@ package de.lubowiecki.aufgaben;
 
 public class Aufgabe1 {
 
-    /* ... */
+    /*
+        Schaue dir die primitiven Datentypen in den KeyNotes an
+        und schreibe für jeden Datentyp 2 Variablendeklarationen.
+    */
 }

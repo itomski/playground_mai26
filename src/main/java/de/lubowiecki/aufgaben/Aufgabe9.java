0f061a28038efd1714f1bd7f9795e4f58fba9b67
tlubowiecki <tomasz@lubowiecki.de>
2026-06-01 12:44:38 +0200

    Aufgabe 9

---
+++ src/main/java/de/lubowiecki/aufgaben/Aufgabe9.java
@@ -0,0 +1,22 @@
+package de.lubowiecki.aufgaben;
+
+public class Aufgabe9 {
+
+
+    /*
+    Übung
+    Schreibe ein Programm, dass das Spiel Pilzsuche realisiert.
+
+    Beim Starten des Programms wird ein 10 Zeilen und 10 Spalten großes Spielfeld erzeugt.
+    Danach werden automatisch 10 „Pilze“ positioniert. Jeder Pilz wird auf einer x/y-Koordinate platziert.
+    Danach hat der Spieler 10 Versuche Zeit so viele Pilze wie möglich durch Eingabe der Koordinaten zu finden.
+
+    Nach 10 Versuchen endet das Spiel und es wird die Anzahl der gefundenen Pilze angezeigt.
+
+    Erweiterung
+    Der Spieler wird gefragt, ob er noch ein weiteres Mal spielen möchte.
+     */
+
+
+
+}

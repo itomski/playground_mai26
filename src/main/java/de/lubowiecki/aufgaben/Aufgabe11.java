29e384fec30eeab6d9834b8506999eb66869c980
tlubowiecki <tomasz@lubowiecki.de>
2026-06-02 14:22:46 +0200

    Aufgabe 11, Update

--- src/main/java/de/lubowiecki/aufgaben/Aufgabe11.java
+++ src/main/java/de/lubowiecki/aufgaben/Aufgabe11.java
@@ -3,7 +3,7 @@ package de.lubowiecki.aufgaben;
 public class Aufgabe11 {
 
     /*
-    Schreibe eine Klasse für einen Logger.
+    Schreibe eine Klasse für einen Logger als ein Singleton.
     Dieser soll beim Aufruf der Methode log(String msg, String level) einen Logeintrag an zentraler Stelle produzieren.
     Die Log-Einträge werden in Form eines Strings nach folgender Vorlage festgehalten:
     Datum/Uhrzeit - Level - Nachricht
@@ -12,12 +12,14 @@ public class Aufgabe11 {
     20.02.2026/17:55:35 - INFO - Methode sowieso wurde aufgerufen
 
     Über die Methode printLog() bekommt man alle Einträge auf ein mal angezeigt.
+    Je ein Eintrag pro Zeile.
 
-    Teste die Klasse.
+    Teste die Klasse, indem du die log-Methode an verschiedenen Stellen deines Programms aufrufst.
 
     Erweiterung:
-    Schreibe eine zusätzliche Methode setSilent(boolean silent) über die man steuern kann, ob eine ausgabe in
-    printLog() auch wirklich auf der Console erfolgt oder unterdrückt wird.
+    Schreibe eine zusätzliche Methode setSilent(boolean silent) über die man steuern kann, ob eine Ausgabe in
+    printLog() auch wirklich auf der Console erfolgt oder unterdrückt werden soll.
+    Der silent-Modus ist per default deaktiviert.
      */
 
 }

9fd627bce9f63f07347dea77322042ac230ac25b
tlubowiecki <tomasz@lubowiecki.de>
2026-06-03 15:23:13 +0200

    Aufgabe 13

---
+++ src/main/java/de/lubowiecki/aufgaben/Aufgabe13.java
@@ -0,0 +1,13 @@
+package de.lubowiecki.aufgaben;
+
+public class Aufgabe13 {
+
+    /*
+    Baue das Programm Produktverwaltrung (Aufabe 8) auf eine ArrayList um.
+
+    Erweitere das Programm, so dass auch ein Löschen von Produkten möglich ist.
+
+    Erweitere das Programm, so dass auch ein Export der Produkte in einer Text-Datei möglich ist.
+    */
+
+}

41ff482361ba217ed549ae1838c2b7910eb7afe6
tlubowiecki <tomasz@lubowiecki.de>
2026-06-08 12:49:48 +0200

    Aufgabe16, Update

--- src/main/java/de/lubowiecki/aufgaben/Aufgabe16.java
+++ src/main/java/de/lubowiecki/aufgaben/Aufgabe16.java
@@ -8,7 +8,8 @@ public class Aufgabe16 {
     Definiere drei Methoden (gibLaut(), bewegDichZum(String ziel), friss(String futter)), die jeweils eine Ausgabe auf der Konsole produzieren.
     Die Ausgabe soll jeweils zu der Tierart passen.
 
-    Finde eine gemeinsame Elternklasse und überlege, welche der Methoden in die Elternklasse verschoben werden können und welche Methoden abstrakt sein sollten.
+    Finde eine gemeinsame Elternklasse und überlege, welche der Methoden in die Elternklasse verschoben werden können und welche Methoden
+    in der Elternklasse abstrakt sein sollten.
 
     Erzeuge eine Liste mit verschiedenen Tieren.
     Laufe dann die Liste Tier für Tier durch und probiere alle 3 Methoden aus.

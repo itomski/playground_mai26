6d912ba5940940fbd8c76ba7e57c8ba20728da05
tlubowiecki <tomasz@lubowiecki.de>
2026-06-08 13:46:03 +0200

    Aufgabe16, Lösung

--- src/main/java/de/lubowiecki/aufgaben/Aufgabe16.java
+++ src/main/java/de/lubowiecki/aufgaben/Aufgabe16.java
@@ -1,5 +1,8 @@
 package de.lubowiecki.aufgaben;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Aufgabe16 {
 
     /*
@@ -13,8 +16,90 @@ public class Aufgabe16 {
 
     Erzeuge eine Liste mit verschiedenen Tieren.
     Laufe dann die Liste Tier für Tier durch und probiere alle 3 Methoden aus.
-
-
     */
 
+    public static void main(String[] args) {
+
+        List<Tier> gehege = new ArrayList<>();
+        gehege.add(new Katze());
+        gehege.add(new Hund());
+        gehege.add(new Affe());
+        gehege.add(new Zebra());
+
+        for(Tier t : gehege) {
+            t.gibLaut();
+            t.friss("Gurken");
+            t.bewegDichZum("Zoo-Ausgang");
+            System.out.println();
+        }
+
+        System.out.println();
+        gibFutter(gehege, "Melone");
+
+        System.out.println();
+        gibFutter(gehege, "Stroh");
+
+    }
+
+    static void gibFutter(List<Tier> gehege, String futter) {
+        for(Tier t : gehege) {
+            t.friss(futter);
+        }
+    }
+}
+
+abstract class Tier {
+
+    abstract void gibLaut();
+
+    void bewegDichZum(String ziel) {
+        System.out.println(getClass().getSimpleName() + " bewegt sich zum " + ziel);
+    }
+
+    void friss(String futter) {
+        System.out.println(getClass().getSimpleName() + " frisst " + futter);
+    }
+}
+
+
+class Hund extends Tier {
+
+    @Override
+    void gibLaut() {
+        System.out.println("Wau wau!");
+    }
+
+    @Override
+    void friss(String futter) {
+        if(futter.equalsIgnoreCase("stroh")) {
+            System.out.println("Hund: Urgh... das fresse ich nicht");
+        }
+        else {
+            super.friss(futter);
+        }
+    }
+}
+
+class Katze extends Tier {
+
+    @Override
+    void gibLaut() {
+        System.out.println("Miau miau!");
+    }
+}
+
+class Affe extends Tier {
+
+    @Override
+    void gibLaut() {
+        System.out.println("Ugh... ugh!");
+    }
+}
+
+class Zebra extends Tier {
+
+    @Override
+    void gibLaut() {
+        System.out.println("Yapee... yappee (soft)");
+    }
 }

abbbeeee6f55778fb91445987f3138a6e2c0a14d
tlubowiecki <tomasz@lubowiecki.de>
2026-06-11 13:36:45 +0200

    Aufgabe 21

---
+++ src/main/java/de/lubowiecki/aufgaben/Aufgabe21.java
@@ -0,0 +1,14 @@
+package de.lubowiecki.aufgaben;
+
+public class Aufgabe21 {
+
+    /*
+
+    Schreibe ein JavaFX Programm für eine Einkaufsliste
+    Über ein Text-Feld können Produkte zu einer ListView hinzugefügt werden.
+    Nach jedem speichern wird die ListView aktualisiert
+
+    */
+
+
+}

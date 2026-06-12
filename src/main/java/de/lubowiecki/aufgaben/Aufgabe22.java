bca1c9c4a62c27f3f97c0f5444057776041d24bd
tlubowiecki <tomasz@lubowiecki.de>
2026-06-12 14:27:45 +0200

    Aufgabe 22

--- src/main/java/de/lubowiecki/aufgaben/Aufgabe22.java
+++ src/main/java/de/lubowiecki/aufgaben/Aufgabe22.java
@@ -3,7 +3,7 @@ package de.lubowiecki.aufgaben;
 public class Aufgabe22 {
 
     /*
-    Schreibe eine eigene Crud-Klasse für ein Produkt.
+    Schreibe eine eigene Crud-Klasse für ein Produkt (name, beschreibung, anzahl, preis).
     Alle Datenbankaktionen sollen in passende Methoden verpackt werden.
     Teste die Klasse :-)
     */

4f6a6f0e4e45645fba4570e37293ab9e724ec5f8
tlubowiecki <tomasz@lubowiecki.de>
2026-06-15 13:33:46 +0200

    Aufgabe 23

---
+++ src/main/java/de/lubowiecki/aufgaben/Aufgabe23.java
@@ -0,0 +1,10 @@
+package de.lubowiecki.aufgaben;
+
+public class Aufgabe23 {
+
+    /*
+    Schreibe ein Repository nach dem Vorbild des TaskRepository für das Speichern einer Person.
+    Verwende dabei das Repository Interface und die DbUtils.
+    */
+
+}

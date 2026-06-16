aa6b98e87b3fd3d156713f420281d4f496cc8a6b
tlubowiecki <tomasz@lubowiecki.de>
2026-06-16 12:45:05 +0200

    Aufgabe 25

---
+++ src/main/java/de/lubowiecki/aufgaben/Aufgabe25.java
@@ -0,0 +1,27 @@
+package de.lubowiecki.aufgaben;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Aufgabe25 {
+
+    /*
+    Schreibe ein Programm zu Verwaltung von Medien.
+    Es gibt verschiedene Medien-Typen. Buch, Video, Musik, Hörspiel
+    Erzeuge eine Liste von 10 Medien unterschhidlicher Typen.
+    Schreibe ein Predicate mit dem du die Liste filtern kannst.
+    Filterung soll nach:
+        Medien-Typ
+        Autor
+        Erscheinungsjahr
+    erfolgen
+    */
+
+    public static void main(String[] args) {
+
+        List<String> namen = new ArrayList<>();
+        //namen.removeIf() // Nimmt ein Predicat entgegen
+        //namen.stream().filter() // Nimmt ein Predicat entgegen
+
+    }
+}

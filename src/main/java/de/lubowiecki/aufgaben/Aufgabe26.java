01575c926d3bf2a7dea61a21022c140d56dfbdbb
tlubowiecki <tomasz@lubowiecki.de>
2026-06-16 15:04:04 +0200

    Aufgabe 26

---
+++ src/main/java/de/lubowiecki/aufgaben/Aufgabe26.java
@@ -0,0 +1,14 @@
+package de.lubowiecki.aufgaben;
+
+public class Aufgabe26 {
+
+    /*
+    Bau eine Liste von 5 Produkten.
+    Verwende dabei die Klasse Produkt aus der Aufgabe 8.
+    Definiere für die Klasse Produkt eine natürliche Ordnung.
+
+    Definiere 3 Comparatoren.
+
+    Sortiere die Liste 3 Mal nach unterschiedlichen Kriterien.
+     */
+}

c4f858bdd6d4be611b25cc90adffd09174267c08
tlubowiecki <tomasz@lubowiecki.de>
2026-06-29 13:50:14 +0200

    Aufgabe 30

---
+++ src/main/java/de/lubowiecki/aufgaben/Aufgabe30.java
@@ -0,0 +1,14 @@
+package de.lubowiecki.aufgaben;
+
+public class Aufgabe30 {
+
+    /*
+    Schreibe eine kleine API zu erfassen einer Kundenliste.
+    Jeder Kunde hat eine Vor- und Nachnamen.
+
+    Es kann die vollständige Kunden-Liste angezeigt werden
+    Es kann ein neuer Kunde hinzugefügt werden
+
+    */
+
+}

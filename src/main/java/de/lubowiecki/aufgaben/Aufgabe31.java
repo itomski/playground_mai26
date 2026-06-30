178ec5188ca2c483a29796f761b96f7477f46759
tlubowiecki <tomasz@lubowiecki.de>
2026-06-30 13:22:24 +0200

    Aufgabe 31

---
+++ src/main/java/de/lubowiecki/aufgaben/Aufgabe31.java
@@ -0,0 +1,14 @@
+package de.lubowiecki.aufgaben;
+
+public class Aufgabe31 {
+
+    /*
+
+    Schreibe eine Controller für eine Website mit 6 Einzelseiten.
+    Baue 2 unterschiedliche Templates die von den Controller-Methoden verwendet werden.
+    Definiere eine Navigation mit der sich der Besucher duch die Seiten bewegen kann.
+    Jede Seite soll unterschiedlichen Inhalt anzeigen
+
+    */
+
+}
